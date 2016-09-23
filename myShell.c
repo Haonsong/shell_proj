ca2bf30dab82c8213cd2fc7c6594cff0055948f3
shanshuang9276 <shanshan1118@gmail.com>
2016-09-22 23:00:44 -0500

    deleting unused code

--- myShell.c
+++ myShell.c
@@ -1,6 +1,6 @@
 
 #include <string.h>
-#include <stdlib.h>
+
 
 #include "shell_loop.h"
 
@@ -11,32 +11,7 @@ int main()
 	printf("My shell is starting...\n" );
 	shell_loop();
 
-<<<<<<< Updated upstream
-	// while (command != "exit")
-	// {
-	// 	if (command == "ls")
-	// 		system("ls");
-	// 	else if (command == "vim")
-	// 		system("vim");
-	// 	else if (command=="date")
-	// 		system("date");
-	// 	else if (command == "help")
-	// 		cout <<"ls\tdate\thelp\texit\n"
-	// 	cout <<"ls\tdate\thelp\texit\n"
-	// 	cout <<"ls\tdate\thelp\texit\n"
-	// }
-}
-=======
-	while (command != "exit")
-	{
-		if (command == "ls")
-			system("ls");
-		else if (command == "vim")
-			system("vim");
-		else if (command=="date")
-			system("date");
-		else if (command == "help")
-			cout <<"ls\tdate\thelp\texit\n"
+
 
 		//redirction ">"
 		//\ls -la /tmp > output" 
@@ -47,4 +22,4 @@ int main()
 	}
 
 }
->>>>>>> Stashed changes
+
