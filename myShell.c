2a51e16803dd5f21b03d22e42729c4b8e21905dc
shanshuang9276 <shanshan1118@gmail.com>
2016-09-22 22:51:20 -0500

    1

--- myShell.c
+++ myShell.c
@@ -11,6 +11,7 @@ int main()
 	printf("My shell is starting...\n" );
 	shell_loop();
 
+<<<<<<< Updated upstream
 	// while (command != "exit")
 	// {
 	// 	if (command == "ls")
@@ -25,3 +26,25 @@ int main()
 	// 	cout <<"ls\tdate\thelp\texit\n"
 	// }
 }
+=======
+	while (command != "exit")
+	{
+		if (command == "ls")
+			system("ls");
+		else if (command == "vim")
+			system("vim");
+		else if (command=="date")
+			system("date");
+		else if (command == "help")
+			cout <<"ls\tdate\thelp\texit\n"
+
+		//redirction ">"
+		//\ls -la /tmp > output" 
+
+		
+		// &
+		// wait
+	}
+
+}
+>>>>>>> Stashed changes
